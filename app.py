6fbfda465f659f416e51526a6d0fe0f3d89a56c7
vivy_phtela <tubasa.0405@icloud.com>
2024-07-29 01:15:25 +0900

    本番環境に変更

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def show_recipes():
         return str(e)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    # serve(app, host='0.0.0.0', port=5000, threads=10)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    serve(app, host='0.0.0.0', port=5000, threads=10)
